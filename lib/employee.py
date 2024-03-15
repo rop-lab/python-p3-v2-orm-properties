9b48e886792b3dec4aa4933b7ce1d5a2b325612f
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-03-15 21:07:32 +0300

    done

--- lib/employee.py
+++ lib/employee.py
@@ -19,6 +19,42 @@ class Employee:
             f"<Employee {self.id}: {self.name}, {self.job_title}, " +
             f"Department ID: {self.department_id}>"
         )
+    @property
+    def name(self, name):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string."
+            )
+    @property
+    def job_title(self):
+        return self._job_title
+    
+    @job_title.setter
+    def job_title(self, job_title):
+        if isinstance(job_title, str) and len(job_title):
+           self._job_title = job_title
+        else:
+            raise ValueError(
+                "job_title must be a non-empty string"
+            )   
+    
+    @property
+    def department_id(self):
+        return self._department_id
+    
+    @department_id.setter
+    def department_id(self, department_id):
+        if type(department_id) is int and Department.find_by_id(department_id):
+            self._department_id = department_id
+        else:
+            raise ValueError(
+                "department_id must reference a department in the database")
 
     @classmethod
     def create_table(cls):
@@ -52,7 +88,7 @@ class Employee:
                 VALUES (?, ?, ?)
         """
 
-        CURSOR.execute(sql, (self.name, self.job_title, self.department_id))
+        CURSOR.execute(sql, (self._name, self._job_title, self._department_id))  # Access attributes directly
         CONN.commit()
 
         self.id = CURSOR.lastrowid
