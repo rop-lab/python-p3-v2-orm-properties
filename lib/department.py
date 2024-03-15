60a641c4d51f48813071f53cda2ce1d1c393c027
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-03-15 21:11:44 +0300

    done

--- lib/department.py
+++ lib/department.py
@@ -1,8 +1,9 @@
-# lib/department.py
 from __init__ import CURSOR, CONN
-from employee import Employee
+
 
 class Department:
+
+    # Dictionary of objects saved to the database.
     all = {}
 
     def __init__(self, name, location, id=None):
@@ -12,28 +13,32 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
-    
+
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, name):
         if isinstance(name, str) and len(name):
             self._name = name
         else:
-            raise ValueError("Name must be a non-empty string.")
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
 
     @property
     def location(self):
         return self._location
-    
+
     @location.setter
     def location(self, location):
         if isinstance(location, str) and len(location):
             self._location = location
-        else: 
-            raise ValueError("Location must be a non-empty string.")
+        else:
+            raise ValueError(
+                "Location must be a non-empty string"
+            )
 
     @classmethod
     def create_table(cls):
@@ -109,11 +114,15 @@ class Department:
     @classmethod
     def instance_from_db(cls, row):
         """Return a Department object having the attribute values from the table row."""
+
+        # Check the dictionary for an existing instance using the row's primary key
         department = cls.all.get(row[0])
         if department:
+            # ensure attributes match row values in case local instance was modified
             department.name = row[1]
             department.location = row[2]
         else:
+            # not in dictionary, create new instance and add to dictionary
             department = cls(row[1], row[2])
             department.id = row[0]
             cls.all[department.id] = department
@@ -126,7 +135,9 @@ class Department:
             SELECT *
             FROM departments
         """
+
         rows = CURSOR.execute(sql).fetchall()
+
         return [cls.instance_from_db(row) for row in rows]
 
     @classmethod
@@ -137,6 +148,7 @@ class Department:
             FROM departments
             WHERE id = ?
         """
+
         row = CURSOR.execute(sql, (id,)).fetchone()
         return cls.instance_from_db(row) if row else None
 
@@ -148,15 +160,20 @@ class Department:
             FROM departments
             WHERE name is ?
         """
+
         row = CURSOR.execute(sql, (name,)).fetchone()
         return cls.instance_from_db(row) if row else None
 
     def employees(self):
         """Return list of employees associated with current department"""
+        from employee import Employee
         sql = """
             SELECT * FROM employees
             WHERE department_id = ?
         """
-        CURSOR.execute(sql, (self.id,))
+        CURSOR.execute(sql, (self.id,),)
+
         rows = CURSOR.fetchall()
-        return [Employee.instance_from_db(row) for row in rows]
+        return [
+            Employee.instance_from_db(row) for row in rows
+        ]
--- lib/employee.py
+++ lib/employee.py
@@ -1,7 +1,5 @@
-# lib/employee.py
 from __init__ import CURSOR, CONN
 from department import Department
-
 
 class Employee:
 
@@ -19,35 +17,37 @@ class Employee:
             f"<Employee {self.id}: {self.name}, {self.job_title}, " +
             f"Department ID: {self.department_id}>"
         )
+
     @property
-    def name(self, name):
+    def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, name):
         if isinstance(name, str) and len(name):
             self._name = name
         else:
             raise ValueError(
-                "Name must be a non-empty string."
+                "Name must be a non-empty string"
             )
+
     @property
     def job_title(self):
         return self._job_title
-    
+
     @job_title.setter
     def job_title(self, job_title):
         if isinstance(job_title, str) and len(job_title):
-           self._job_title = job_title
+            self._job_title = job_title
         else:
             raise ValueError(
                 "job_title must be a non-empty string"
-            )   
-    
+            )
+
     @property
     def department_id(self):
         return self._department_id
-    
+
     @department_id.setter
     def department_id(self, department_id):
         if type(department_id) is int and Department.find_by_id(department_id):
@@ -88,7 +88,7 @@ class Employee:
                 VALUES (?, ?, ?)
         """
 
-        CURSOR.execute(sql, (self._name, self._job_title, self._department_id))  # Access attributes directly
+        CURSOR.execute(sql, (self.name, self.job_title, self.department_id))
         CONN.commit()
 
         self.id = CURSOR.lastrowid
